3679e7bb9344f7c778e9726a7e1fdc05a4a0321e
yuki3738 <y.minamiya@gmail.com>
2018-04-07 12:53:14 +0900

    ActionMailer の設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,4 +56,13 @@ Rails.application.configure do
     host: 'localhost',
     port: 3000
   }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    authentication: :plain,
+    user_name: Rails.application.secrets.SMTP_EMAIL,
+    password: Rails.application.secrets.SMTP_PASSWORD
+  }
 end
